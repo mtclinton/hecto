2847ab7c289141d6c5fa48f4dcc916fc524e0aa7
Max Clinton <maxtclinton@gmail.com>
2023-03-16 21:00:12 -0400

    Basic document viewing

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,12 @@
+use crate::Document;
+use crate::Row;
 use crate::Terminal;
+use std::env;
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -12,6 +16,7 @@ pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
+    document: Document,
 }
 
 impl Editor {
@@ -29,16 +34,24 @@ impl Editor {
         }
     }
     pub fn default() -> Self {
-        Self{ 
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
+        } else {
+            Document::default()
+        };
+        Self {
             should_quit: false,
-            terminal: Terminal::default().expect("Failed to initialize terminal"),            
-            cursor_position: Position { x: 0, y: 0 }, 
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+            document,
+            cursor_position: Position::default(),
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(&Position{x:0, y:0});
+        Terminal::cursor_position(&Position::default());
         if self.should_quit {
             Terminal::clear_screen();
             println!("Goodbye.\r");
@@ -49,24 +62,22 @@ impl Editor {
         Terminal::cursor_show();
         Terminal::flush()
     }
-
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
-            Key::Up 
-            | Key::Down 
-            | Key::Left 
+            Key::Up
+            | Key::Down
+            | Key::Left
             | Key::Right
             | Key::PageUp
             | Key::PageDown
-            | Key::Home
-            | Key::End => self.move_cursor(pressed_key),
+            | Key::End
+            | Key::Home => self.move_cursor(pressed_key),
             _ => (),
         }
         Ok(())
     }
-
     fn move_cursor(&mut self, key: Key) {
         let Position { mut y, mut x } = self.cursor_position;
         let size = self.terminal.size();
@@ -93,7 +104,6 @@ impl Editor {
         }
         self.cursor_position = Position { x, y }
     }
-
     fn draw_welcome_message(&self) {
         let mut welcome_message = format!("Hecto editor -- version {}", VERSION);
         let width = self.terminal.size().width as usize;
@@ -104,12 +114,19 @@ impl Editor {
         welcome_message.truncate(width);
         println!("{}\r", welcome_message);
     }
-    
+    pub fn draw_row(&self, row: &Row) {
+        let start = 0;
+        let end = self.terminal.size().width as usize;
+        let row = row.render(start, end);
+        println!("{}\r", row)
+    }
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for row in 0..height-1 {
+        for terminal_row in 0..height - 1 {
             Terminal::clear_current_line();
-            if row == height / 3 {
+            if let Some(row) = self.document.row(terminal_row as usize) {
+                self.draw_row(row);
+            } else if self.document.is_empty() && terminal_row == height / 3 {
                 self.draw_welcome_message();
             } else {
                 println!("~\r");
@@ -120,5 +137,5 @@ impl Editor {
 
 fn die(e: std::io::Error) {
     Terminal::clear_screen();
-    panic!("{}", e);
+    panic!("{}",e);
 }
