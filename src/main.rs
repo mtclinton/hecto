dae21a2d25e69c21285de223bb9ae4976fcb47d1
Max Clinton <maxtclinton@gmail.com>
2023-03-19 22:01:33 -0400

    Move left

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ mod terminal;
 use editor::Editor;
 pub use document::Document;
 pub use editor::Position;
+pub use editor::SearchDirection;
 pub use row::Row;
 pub use terminal::Terminal;
 fn main() {
