d53b3a45625184d68e8e9fb8e7663889fa9b2e30
Max Clinton <maxtclinton@gmail.com>
2023-03-25 00:02:40 -0400

    colorize chars

--- src/row.rs
+++ src/row.rs
@@ -204,21 +204,32 @@ impl Row {
             } else {
                 &highlighting::Type::None
             };
-            if opts.numbers() {
-                if (c.is_ascii_digit()
-                    && (prev_is_separator || *previous_highlight == highlighting::Type::Number))
-                    || (*c == '.' && *previous_highlight == highlighting::Type::Number)
-                {
-                    highlighting.push(highlighting::Type::Number);
-                } else {
-                    highlighting.push(highlighting::Type::None);
-                }
-            } else {
+            if opts.characters() && !in_string && *c == '\'' {
+                prev_is_separator = true;
+                if let Some(next_char) = chars.get(index.saturating_add(1)) {
+                    let closing_index = if *next_char == '\\' {
+                        index.saturating_add(3)
+                    } else {
+                        index.saturating_add(2)
+                    };
+                    if let Some(closing_char) = chars.get(closing_index) {
+                        if *closing_char == '\'' {
+                            for _ in 0..=closing_index.saturating_sub(index) {
+                                highlighting.push(highlighting::Type::Character);
+                                index += 1;
+                            }
+                            continue;
+                        }
+                    }
+                };
                 highlighting.push(highlighting::Type::None);
+                index += 1;
+                continue;
             }
             if opts.strings() {
                 if in_string {
                     highlighting.push(highlighting::Type::String);
+
                     if *c == '\\' && index < self.len().saturating_sub(1) {
                         highlighting.push(highlighting::Type::String);
                         index += 2;
@@ -239,6 +250,18 @@ impl Row {
                     index += 1;
                     continue;
                 }
+            }
+            if opts.numbers() {
+                if (c.is_ascii_digit()
+                    && (prev_is_separator || *previous_highlight == highlighting::Type::Number))
+                    || (*c == '.' && *previous_highlight == highlighting::Type::Number)
+                {
+                    highlighting.push(highlighting::Type::Number);
+                } else {
+                    highlighting.push(highlighting::Type::None);
+                }
+            } else {
+                highlighting.push(highlighting::Type::None);
             }
             prev_is_separator = c.is_ascii_punctuation() || c.is_ascii_whitespace();
             index += 1;
